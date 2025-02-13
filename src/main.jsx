21d15d985db16a0a6a59236491bffe6fe10e9901
Udaykumar Dhokia <Dhokia.Udaykumar.22co@iitram.ac.in>
2025-02-13 10:36:51 +0530

    Fixing Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "../src/redux/store.js";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
