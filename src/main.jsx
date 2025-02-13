4113a506d236706290d6491851e7470ec5d08997
Udaykumar Dhokia <Dhokia.Udaykumar.22co@iitram.ac.in>
2025-02-13 10:34:41 +0530

    Enable Vercel Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "../src/redux/store.js";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <App />
+      <SpeedInsights />
     </Provider>
   </StrictMode>
 );
